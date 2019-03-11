1e2ced9b1daed9fb09e8b68a217081e32feed6b4
nrista-org <narista.sa@gmail.com>
2019-03-11 17:56:38 +0700

    adding POJOs. Closes #2.

---
+++ src/main/java/com/example/sfgpetclinic/model/Person.java
@@ -0,0 +1,24 @@
+package com.example.sfgpetclinic.model;
+
+public class Person {
+
+    private String firstName;
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastname() {
+        return lastname;
+    }
+
+    public void setLastname(String lastname) {
+        this.lastname = lastname;
+    }
+
+    private String lastname;
+}
